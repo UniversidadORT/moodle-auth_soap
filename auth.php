192ceeb423144a09fc79c00153d11eb57abfc2dd
Jean Pierre Ducassou <ducassou@ort.edu.uy>
2012-05-22 13:33:06 -0300

    Moodle SOAP authentication plugin first commit based on 'none' plugin.

---
+++ auth.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * @author Jean Pierre Ducassou
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package moodle multiauth
+ *
+ * Authentication Plugin: Authentication throught SOAP webservice
+ *
+ */
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+}
+
+require_once($CFG->libdir.'/authlib.php');
+
+/**
+ * Plugin for SOAP authentication.
+ */
+class auth_plugin_soap extends auth_plugin_base {
+
+    /**
+     * Constructor.
+     */
+    function auth_plugin_soap() {
+        $this->authtype = 'soap';
+        $this->config = get_config('auth/soap');
+    }
+
+    /**
+     * Returns true if the username and password work or don't exist and false
+     * if the user exists and the password is wrong.
+     *
+     * @param string $username The username
+     * @param string $password The password
+     * @return bool Authentication success or failure.
+     */
+    function user_login ($username, $password) {
+        global $CFG, $DB;
+	return true;
+/*
+        if ($user = $DB->get_record('user', array('username'=>$username, 'mnethostid'=>$CFG->mnet_localhost_id))) {
+            return validate_internal_user_password($user, $password);
+        }
+        return true;
+*/
+    }
+
+    /**
+     * Updates the user's password.
+     *
+     * called when the user password is updated.
+     *
+     * @param  object  $user        User table object
+     * @param  string  $newpassword Plaintext password
+     * @return boolean result
+     *
+     */
+    function user_update_password($user, $newpassword) {
+/*
+        $user = get_complete_user_data('id', $user->id);
+        return update_internal_user_password($user, $newpassword);
+*/
+    }
+
+    function prevent_local_passwords() {
+        return false;
+    }
+
+    /**
+     * Returns true if this authentication plugin is 'internal'.
+     *
+     * @return bool
+     */
+    function is_internal() {
+        return true;
+    }
+
+    /**
+     * Returns true if this authentication plugin can change the user's
+     * password.
+     *
+     * @return bool
+     */
+    function can_change_password() {
+        return false;
+    }
+
+    /**
+     * Returns the URL for changing the user's pw, or empty if the default can
+     * be used.
+     *
+     * @return moodle_url
+     */
+    function change_password_url() {
+        return null;
+    }
+
+    /**
+     * Returns true if plugin allows resetting of internal password.
+     *
+     * @return bool
+     */
+    function can_reset_password() {
+        return false;
+    }
+
+    /**
+     * Prints a form for configuring this authentication plugin.
+     *
+     * This function is called from admin/auth.php, and outputs a full page with
+     * a form for configuring this plugin.
+     *
+     * @param array $page An object containing all the data for this page.
+     */
+    function config_form($config, $err, $user_fields) {
+        include "config.html";
+    }
+
+    /**
+     * Processes and stores configuration data for this authentication plugin.
+     */
+    function process_config($config) {
+        return true;
+    }
+
+}
+
+
